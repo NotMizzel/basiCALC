8101d7e46ac34599839c051232296eac583e4251
NotMizzel <heybray38@gmail.com>
2022-09-23 19:02:19 -0500

    Unstable

--- basiCALC.v2.unstablebuild.py
+++ basiCALC.v2.unstablebuild.py
@@ -4,16 +4,25 @@ while True:
     input_div = "/"
     input_add = "+"
     input_sub = "-"
+    inputequals = "="
     first = float(input('Please enter your first value: '))
     operator = input('Please enter your operator: ')
     second =  float(input('Please enter your second value: '))
-    if first == float(int(first)):
+    both = (first and second)
+    bothint = int(both)
+    oneint = int(first)
+    twoint = int(second)
+    if first == int(first) and operator == input_add:
         print(int(first + second))
-        if operator == input_mult:
-         print(first * second)
-    if operator == input_div:
+    if both != bothint or first != oneint or second != twoint or float(first) + float(second) != bothint and operator == input_mult:
+        print(float(first * second))
+    if both != bothint or first != oneint or second != twoint or float(first) + float(second) != bothint and operator == input_div:
         print(first / second)
-    if operator == input_add and first == float and second == float or first == float(int(first)) and second == float or second == float and first == float(int(second)):
+    if operator == input_add and both != bothint or first != oneint or second != twoint or float(first) + float(second) != bothint:
         print(first + second)
-    if operator == input_sub:
+    if both != bothint or first != oneint or second != twoint or float(first) + float(second) != bothint and operator == input_sub:
         print(first - second)
+    if operator == inputequals and first == second:
+        print ("true")
+    if operator == inputequals and first != second:
+        print("false")
