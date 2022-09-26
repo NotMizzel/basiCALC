e825c48c07a59121d3408cd6874f1669ce8a0bc7
NotM1zzel <heybray38@gmail.com>
2022-09-26 14:00:34 -0500

    Added a way to not print a floating point

--- basiCALC.v2.unstablebuild.py
+++ basiCALC.v2.unstablebuild.py
@@ -7,24 +7,25 @@ while True:
     input_sub = "-"
     inputequals = "="
     first = float(input('Please enter your first value: '))
-    einput = (input(" "))
-    if einput == "x":
-        print(input("exponent?"))
     operator = input('Please enter your operator: ')
     second =  float(input('Please enter your second value: '))
     both = (first and second)
     bothint = int(both)
     oneint = int(first)
     twoint = int(second)
-    if both == bothint and operator == input_add:
+    suma = (first + second)
+    sumd = (first / second)
+    summ = (first * second)
+    sums = (first - second)
+    if both == bothint or first + second == int(suma) and operator == input_add:
         print(int(first + second))
-    if both != bothint or first != oneint or second != twoint or float(first) + float(second) != bothint and operator == input_mult:
-        print(float(first * second))
-    if both != bothint or first != oneint or second != twoint or float(first) + float(second) != bothint and operator == input_div:
+    if operator == input_mult and first != oneint and second != twoint:
+        print(first * second)
+    if operator == input_div and first != oneint and second != twoint:
         print(first / second)
-    if operator == input_add and both != bothint or first != oneint or second != twoint or float(first) + float(second) != bothint:
+    if operator == input_add and both != bothint or first != oneint or second != twoint and suma == first + second != int(suma):
         print(first + second)
-    if both != bothint or first != oneint or second != twoint or float(first) + float(second) != bothint and operator == input_sub:
+    if operator == input_sub and first != oneint and second != twoint:
         print(first - second)
     if operator == inputequals and first == second:
         print ("true")
