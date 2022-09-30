425e3b7ca746c0c51e1a15c4b2bef05064b4f7ba
NotMizzel <113567858+NotMizzel@users.noreply.github.com>
2022-09-30 11:25:40 -0500

    Changed code a little

--- basiCALC.v2.unstablebuild.py
+++ basiCALC.v2.unstablebuild.py
@@ -17,15 +17,21 @@ while True:
     sumd = (first / second)
     summ = (first * second)
     sums = (first - second)
-    if both == bothint or first + second == int(suma) and operator == input_add:
+    if both == bothint or first / second == int(sumd) and operator == input_div:
+        print(int(first - second))
+    if both == bothint or first - second == int(sums) and operator == input_sub:
+        print(int(first - second))
+    if both == bothint or first * second == int(summ) and operator == input_mult:
+        print(int(first * second))
+    if operator == input_add and both == bothint or first + second == int(suma):
         print(int(first + second))
-    if operator == input_mult and first != oneint and second != twoint:
+    if operator == input_mult and both != bothint or first != oneint and second != twoint and summ == first * second != int(summ):
         print(first * second)
-    if operator == input_div and first != oneint and second != twoint:
+    if operator == input_div and both != bothint or first != oneint and second != twoint and sumd == first / second != int(sumd):
         print(first / second)
-    if operator == input_add and both != bothint or first != oneint or second != twoint and suma == first + second != int(suma):
+    if operator == input_add and both != bothint or first != oneint or second != twoint and suma == first + second != int(suma) and operator == input_add:
         print(first + second)
-    if operator == input_sub and first != oneint and second != twoint:
+    if operator == input_sub and both != bothint or first != oneint and second != twoint and sums == first - second != int(sums):
         print(first - second)
     if operator == inputequals and first == second:
         print ("true")
